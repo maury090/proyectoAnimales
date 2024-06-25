9251ca89d0e2ce367c69a987221a95147dd5dead
maury090 <129112305+maury090@users.noreply.github.com>
2024-06-25 18:54:50 -0400

    Initial commit

---
+++ animales/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context={}
+    return render(request, 'animales/home.html',context)
+
+def adopcion_perrito(request):
+    context={}
+    return render(request, 'animales/adopcion_perritos.html',context)
