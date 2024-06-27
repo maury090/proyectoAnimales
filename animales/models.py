4c4ea3ef7089c45b76cba1bd7d734f0fe0138440
maury090 <129112305+maury090@users.noreply.github.com>
2024-06-26 22:51:07 -0400

    orm
    guardado antes de ejecutar prueba orm

--- animales/models.py
+++ animales/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ayuda(models.Model)
+
+class producto(models.Model):
+
+    id_producto = models.CharField(primary_key=True, max_length=6)
+    nombre_producto = models.CharField(max_length=50)
+    marca_producto = models.CharField(max_length=20)
+    cantidad_necesaria_prod = models.CharField(max_length=2)
+    valor_producto = models.CharField(max_length=5)
+
+    def __str__(self):
+        return str(self.nombre_producto)+""+str(self.marca_producto)
