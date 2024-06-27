0dcb5a7d0e4373b679814178192c754f98bc9b82
maury090 <129112305+maury090@users.noreply.github.com>
2024-06-26 23:07:49 -0400

    orm creado

--- animales/models.py
+++ animales/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
-class ayuda(models.Model)
+class ayuda(models.Model):
+
+    id_ayuda = models.AutoField(db_column='idAyuda', primary_key=True)
+    ayuda = models.CharField(max_length=20, blank=False, null=False)
+
+    def _str_(self):
+        return str(self.ayuda)
 
 class producto(models.Model):
 
@@ -10,6 +16,7 @@ class producto(models.Model):
     marca_producto = models.CharField(max_length=20)
     cantidad_necesaria_prod = models.CharField(max_length=2)
     valor_producto = models.CharField(max_length=5)
+    id_ayuda = models.ForeignKey('Ayuda', on_delete=models.CASCADE, db_column='id_ayuda')
 
     def __str__(self):
         return str(self.nombre_producto)+""+str(self.marca_producto)
